e4254185c78d5efeeb8f0e9247dccc8ba6024c6c
serifeaydin <serifecevik93@gmail.com>
2024-04-28 00:57:46 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        urgent: '#d4d7ff',
+
+        normal: '#ffd9d4',
+      },
+    },
+  },
+  plugins: [],
+};
